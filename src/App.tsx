db694c496888b31023acc2f81a1dba4c51d3ba3c
Niklesh Raut <nraut@pypestream.com>
2023-09-16 18:27:03 +0530

    BarGraph component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import Markdown from "./projects/markdown/Markdown";
 // import DrumMachine from "./projects/drumMachine/DrumMachine";
 // import Calculator from "./projects/calculator/Calculator";
+// import TwetnyFivePlusFive from "./projects/twentyFivePlusFive/TwentyFivePlusFive";
 
-import TwetnyFivePlusFive from "./projects/twentyFivePlusFive/TwentyFivePlusFive";
+import BarGraph from "./projects/barGraph/BarGraph";
 
 function App() {
   return (
     <>
-      <TwetnyFivePlusFive />
+      <BarGraph />
     </>
   );
 }
